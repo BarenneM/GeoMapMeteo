b1193ae11a8642da6a8529df11b72dea9fde1671
BarenneM <barenne.marie@outlook.com>
2021-03-11 09:22:01 +0100

    ajout de css et bootstrap

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 
 const boutton = document.querySelector("#bouton");
 const temp = document.querySelector("#temp");
-const tableauTemperatures = [];
-const tableauDates = [];
 const keyword = document.querySelector('#keyword');
 const villes  = document.querySelector('#villes');
+let tableauTemperatures = [];
+let tableauDates = [];
 
 
 async function fetchMeteo(ville) {
@@ -22,6 +22,9 @@ async function fetchMeteo(ville) {
     const url5Jours = `http://api.openweathermap.org/data/2.5/forecast?q=${ville}&appid=1ce64129da731d692308f766613a1037`;
     const response2 = await fetch(url5Jours);
     const temp5Jours = await response2.json();
+    tableauTemperatures = [];
+    tableauDates = [];
+
     // console.log(temp5Jours);
     temp5Jours.list.forEach((index) => {
         let heure = index.dt_txt.split(" ");
@@ -34,55 +37,10 @@ async function fetchMeteo(ville) {
 }
 
 keyword.addEventListener('keyup', (event) => {
-    //console.log(event.currentTarget.value);
-    villes.innerHTML='';
-    const url = 'https://places-dsn.algolia.net/1/places/query';
-    
-
-    fetch(url, {
-        method: 'POST',
-        body: JSON.stringify({query: event.currentTarget.value })
-    })
-    .then(response => response.json())
-    .then((data) => {
-        const cityList = data.hits;
-        const displayedCity = [];
-        var displedName = "";
-        let firstWord;
-
-        cityList.forEach(ville => {
-            const cityName = ville.locale_names.default[0];
-
-            const splittedName = cityName.split(" ", 2);
-            firstWord = splittedName[0];
-
-            displedName = firstWord + " (" + ville.country_code + ")";
-            
-            if (!(displayedCity.includes(displedName))) {
-                displayedCity.push(displedName);
-                villes.insertAdjacentHTML('beforeend', `<option value="${displedName}">`);
-            };
-        });
-        
-        const options = document.querySelectorAll('#villes option');
-        options.forEach(option => {
-            
-            keyword.addEventListener('change', (event) => {                
-                cityList.forEach(city => {                    
-                    var res = keyword.value.split(" ",2);
-                    firstWord = res[0];            
-                });
-            });
-        });
-
-        keyword.addEventListener('change', (event) => {
-            var villeCode = keyword.value.split(" ",2);
-                const premierMot = villeCode[0];
-                console.log(premierMot);
-                fetchMeteo(premierMot);
-        } )
-    })
-    .catch((e) => {
-        console.log(e);
-    })
+    autoComplete();
 });
+
+
+
+
+//keyCode === 13 -> touche 'entrer' du clavier
